da2e26b5894e14d455bf9ea3a5abf292070dcae9
aniruddha <ani@dctacademy.com>
2020-08-26 16:51:14 +0530

    express json

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const configureDB = require('./config/database')
 const router = require('./config/routes')
 const port = 3050
 configureDB()
+app.use(express.json())
 app.use(router)
 
 app.listen(port, () => {
